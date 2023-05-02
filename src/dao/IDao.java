343bea8bd2f027567d32fd1f68c1fdd5c27d40c8
TmaneChouaib <chouaib.tmane@hotmail.com>
2023-05-02 23:56:52 +0100

    refomulation de la doc

--- src/dao/IDao.java
+++ src/dao/IDao.java
@@ -2,5 +2,7 @@ package dao;
 
 public interface IDao {
     //On peut supprimer public, car toutes les methods d'une interface sont par default de types publics.
+    //méthode qui retourne de la donnée
     double getData();
+
 }
