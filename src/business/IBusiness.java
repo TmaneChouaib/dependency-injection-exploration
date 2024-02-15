3c5e05c04316071164c22e520efa4eb82b244b92
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-02-15 23:58:02 +0100

    Added IBusinessImpl class implementing IBusiness interface with data calculation logic

---
+++ src/business/IBusiness.java
@@ -0,0 +1,5 @@
+package business;
+
+public interface IBusiness {
+    double calculate();
+}
---
+++ src/business/IBusinessImpl.java
@@ -0,0 +1,19 @@
+package business;
+
+import dao.IDao;
+
+public class IBusinessImpl implements IBusiness {
+
+    private IDao dao;
+
+    @Override
+    public double calculate() {
+        double temp = dao.getData();
+        double result = temp * 10;
+        return result;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
